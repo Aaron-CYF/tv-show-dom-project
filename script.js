ff179366d0c30d066efed5d6cc34d96ed952a30f
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2021-10-26 17:42:05 +0100

    created dummy card

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+import "./episodes.js";
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -9,4 +11,18 @@ function makePageForEpisodes(episodeList) {
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
-window.onload = setup;
+// window.onload = setup;
+
+window.onload = () => {
+let episode = getOneEpisode();
+let epiCard = document.querySelector(".epi-card");
+let cardTitle = epiCard.querySelector(".title");
+let cardImage = epiCard.querySelector(".img-preview");
+let cardSumm = epiCard.querySelector(".summary");
+let img = document.createElement("img");
+
+cardTitle.textContent = episode.name;
+img.src = episode.image.medium;
+cardImage.append(img);
+cardSumm.innerHTML = episode.summary;
+};
