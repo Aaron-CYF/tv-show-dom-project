d459cae1cefc817db804e994a81645f7f55b9669
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2021-10-26 19:10:04 +0100

    loaded all content

--- script.js
+++ script.js
@@ -9,20 +9,43 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  let epiCardCopy = document.querySelector(".epi-card");
+  let main = document.querySelector('main');
+  main.innerHTML = "";
+
+  episodeList.forEach(epi => {
+    let epiCard = epiCardCopy.cloneNode(true);
+    console.log(epiCardCopy);
+    let cardTitle = epiCard.querySelector(".title");
+    let cardImage = epiCard.querySelector(".img-preview");
+    let cardSumm = epiCard.querySelector(".summary");
+    let img = document.createElement("img");
+
+    cardTitle.textContent = `${epi.name} - S${('0' + epi.season).slice(-2)}${('0' + epi.number).slice(-2)}`;
+    img.src = epi.image.medium;
+    cardImage.append(img);
+    cardSumm.innerHTML = epi.summary;
+
+    main.append(epiCard);
+    
+  });
 }
 
-// window.onload = setup;
 
-window.onload = () => {
-let episode = getOneEpisode();
-let epiCard = document.querySelector(".epi-card");
-let cardTitle = epiCard.querySelector(".title");
-let cardImage = epiCard.querySelector(".img-preview");
-let cardSumm = epiCard.querySelector(".summary");
-let img = document.createElement("img");
 
-cardTitle.textContent = episode.name;
-img.src = episode.image.medium;
-cardImage.append(img);
-cardSumm.innerHTML = episode.summary;
-};
+// window.onload = () => {
+// let episode = getOneEpisode();
+// let epiCard = document.querySelector(".epi-card");
+// let cardTitle = epiCard.querySelector(".title");
+// let cardImage = epiCard.querySelector(".img-preview");
+// let cardSumm = epiCard.querySelector(".summary");
+// let img = document.createElement("img");
+
+// cardTitle.textContent = episode.name;
+// img.src = episode.image.medium;
+// cardImage.append(img);
+// cardSumm.innerHTML = episode.summary;
+// };
+
+window.onload = setup;
