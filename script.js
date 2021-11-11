00246828788f96c26c70b503245df62a69c2b5b3
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2021-11-11 20:13:48 +0000

    separated out card display function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import "./episodes.js";
 import "./shows.js"
 //You can edit ALL of the code here
 const allEpisodes = getAllEpisodes();
+let main = document.querySelector("main");
 let searchBox = document.querySelector("#search-box");
 let selectBox = document.querySelector("#select-box");
 let filterText = document.querySelector("#filter-text");
@@ -9,39 +10,81 @@ let resetSearch = document.querySelector("#search-reset");
 let epiCardCopy;
 let showCardCopy;
 
+
 function setup() {
   epiCardCopy = document.querySelector(".epi-card");
   showCardCopy = document.querySelector(".show-card")
-  makePageForEpisodes(allEpisodes);
+  makePageMainContent(allEpisodes);
   searchBox.addEventListener("input", filterEpisodes);
   selectBox.addEventListener("change", chooseEpisode);
   resetSearch.addEventListener("click", doSearchReset);
 }
 
+
 function filterEpisodes(event) {
-  makePageForEpisodes(allEpisodes.filter(epi => {
+  makePageMainContent(allEpisodes.filter(epi => {
     return epi.name.toLowerCase().includes(event.target.value.toLowerCase()) || epi.summary.toLowerCase().includes(event.target.value.toLowerCase());
   }));
 }
 
+
 function chooseEpisode(event) {
   if (event.target.value == 0) {
-    makePageForEpisodes(allEpisodes);
+    makePageMainContent(allEpisodes);
   } else {
-    makePageForEpisodes([allEpisodes.find(epi => epi.id == event.target.value)])
+    makePageMainContent([allEpisodes.find(epi => epi.id == event.target.value)])
   }
 }
+
 
 function doSearchReset() {
   selectBox.children[0].selected = "selected";
   searchBox.value = "";
-  makePageForEpisodes(allEpisodes);
+  makePageMainContent(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  let main = document.querySelector("main");
+
+function makePageMainContent(data) {
   main.innerHTML = "";
   selectBox.innerHTML = "";
+
+  makePageForEpisodes(data);
+  fillSelectBox(data);
+}
+
+
+function createEpisodeIdentifier(episode = getOneEpisode()) {
+  let season = "0" + episode.season;
+  let epiNumber = "0" + episode.number;
+
+  season = "S" + season.slice(-2);
+  epiNumber = "E" + epiNumber.slice(-2);
+  return season + epiNumber;
+}
+
+
+function fillSelectBox(contentList) {
+
+  contentList.forEach(content => {
+    let option = document.createElement("option");
+
+    option.innerText = `${createEpisodeIdentifier(content)} - ${content.name}`;
+    option.value = content.id;
+  
+    if (selectBox.innerHTML === "") {
+      selectBox.innerHTML = "<option value='0'></option>";
+    }
+
+    selectBox.append(option);
+  });
+
+  if (contentList.length === 1) {
+    selectBox.children[1].selected = "selected";
+  }
+}
+
+
+function makePageForEpisodes(episodeList) {
 
   filterText.textContent = `Showing ${episodeList.length} episode(s) out of ${allEpisodes.length}`
 
@@ -51,26 +94,13 @@ function makePageForEpisodes(episodeList) {
     let cardImage = epiCard.querySelector(".img-preview");
     let cardSumm = epiCard.querySelector(".summary");
     let img = document.createElement("img");
-    let option = document.createElement("option");
 
-    cardTitle.textContent = `${epi.name} - S${("0" + epi.season).slice(-2)}E${("0" + epi.number).slice(-2)}`;
+    cardTitle.textContent = `${epi.name} - ${createEpisodeIdentifier(epi)}`;
     img.src = epi.image.medium;
     cardImage.append(img);
     cardSumm.innerHTML = epi.summary;
 
-    option.innerText = `S${("0" + epi.season).slice(-2)}E${("0" + epi.number).slice(-2)} - ${epi.name}`;
-    option.value = epi.id;
-
     main.append(epiCard);
-    if(selectBox.innerHTML === "") {
-      selectBox.innerHTML = "<option value='0'></option>";
-    }
-
-    selectBox.append(option);
-    if (episodeList.length === 1) {
-      selectBox.children[1].selected = "selected";
-    }
-    
   });
 }
 
